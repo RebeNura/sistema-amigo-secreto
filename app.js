fc14c31dc72c129936ccf1f4a7d4adb85153179a
Rebeca <rsnr2015@gmail.com>
2025-01-29 23:07:10 -0600

    Actualizacion basica de todas las funciones

--- app.js
+++ app.js
@@ -1,20 +1,75 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let arrayNombresAmigos=[]
+let arrayNombresAmigos = []
 
 
-function agregarAmigo(){
+function agregarAmigo() {
 
-    let nombreAmigo = document.getElementById("amigo" ).value;
-    console.log(nombreAmigo,nombreAmigo.length);
-    if(nombreAmigo.length==0){
-        alert("Por favor, inserte un nombre.")}
+    let nombreAmigo = document.getElementById("amigo").value;
+    console.log(nombreAmigo, nombreAmigo.length);
+    if (nombreAmigo.length == 0) {
+        alert("Por favor, inserte un nombre.")
+    }
 
-    else{
+    else {
         arrayNombresAmigos.push(nombreAmigo);
         console.log(arrayNombresAmigos);
-        document.querySelector("#amigo").value="";
+        document.querySelector("#amigo").value = "";
+        actualizarListaAmigos();
+
+
+    }
+
+    return;
+
+
+}
+
+
+function actualizarListaAmigos() {
+
+    let lista = document.querySelector("#listaAmigos");
+    lista.innerHTML = ""
+
+
+    for (let elemento in arrayNombresAmigos) {
+
+        console.log(arrayNombresAmigos[elemento]);
+        const li = document.createElement('li');
+        li.textContent = arrayNombresAmigos[elemento];
+        lista.appendChild(li);
+    }
+
+
+
+
+    return;
+}
+
+
+function sortearAmigo(){
+
+
+    console.log(arrayNombresAmigos.length)
+    if (arrayNombresAmigos.length == 0) {
+        alert("Por favor, inserte un nombre.")
+    }
+
+    else{
+
+        let indiceAleatorio=Math.floor((Math.random() * arrayNombresAmigos.length-1)) + 1;
+        console.log(arrayNombresAmigos[indiceAleatorio])
+
+        let lista = document.querySelector("#listaAmigos");
+        lista.innerHTML = "";
+
+        let listaSorteo=document.querySelector("#resultado" )
+        listaSorteo.innerHTML=arrayNombresAmigos[indiceAleatorio]
+        
+
+
     }
 
 
 }
+
