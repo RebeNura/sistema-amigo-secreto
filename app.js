7f769a45d1b90ee2d260301d71e3c3cb878ae945
Rebeca <rsnr2015@gmail.com>
2025-01-29 22:13:36 -0600

    Primera parte completa

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let arrayNombresAmigos=[]
+
+
+function agregarAmigo(){
+
+    let nombreAmigo = document.getElementById("amigo" ).value;
+    console.log(nombreAmigo,nombreAmigo.length);
+    if(nombreAmigo.length==0){
+        alert("Por favor, inserte un nombre.")}
+
+    else{
+        arrayNombresAmigos.push(nombreAmigo);
+        console.log(arrayNombresAmigos);
+        document.querySelector("#amigo").value="";
+    }
+
+
+}
